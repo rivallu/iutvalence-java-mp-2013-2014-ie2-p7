d46f02c66c97fd1cca313bb944a1a972ae2a5747
sebastienjean <baz.jean@gmail.com>
2013-09-19 21:44:42 +0200

    Added some TODOs
    These TODOs must be processed before doing anything else

--- src/fr/iutvalence/java/mp/Fate_Emblem/FateEmblem.java
+++ src/fr/iutvalence/java/mp/Fate_Emblem/FateEmblem.java
@@ -3,6 +3,7 @@ package fr.iutvalence.java.mp.Fate_Emblem;
 
 /**
  * @author rivallu
+ * // TODO (fix) Misplaced comment (must be placed before @author)
  *to be continued...
  */
 public class FateEmblem
